0ca85f23548a9cad04635a0c10aa3705d14ed749
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-12-10 11:12:23 +0530

    Uses import guard

--- tables.py
+++ tables.py
@@ -1,9 +1,14 @@
-print "enter a number for multipilication table"
-m=int(raw_input())
-print"enter a limit"
-n=int(raw_input())
 def multi (x,y):
     for i in range (1,y+1):
         print("{} * {} = {}".format(x,i,x*i))
 
-multi(m,n)
+def main():
+    print "enter a number for multipilication table"
+    m=int(raw_input())
+    print"enter a limit"
+    n=int(raw_input())
+    multi(m,n)
+
+
+if __name__ == "__main__":
+    main()
