a68a620ed891044fe0057c5c0f5755a69213c054
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-12-10 11:18:28 +0530

    No need for lists here. Strings iterate fine.

--- panagram.py
+++ panagram.py
@@ -1,9 +1,7 @@
 def panagram(x):
-    s=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    for i in s:
+    for i in 'abcdefghijklmnopqrstuvwxyz':
         if i not in x:
             return False
-        
     return True
 
 def main():
